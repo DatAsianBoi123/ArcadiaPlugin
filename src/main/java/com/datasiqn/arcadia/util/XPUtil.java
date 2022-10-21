4d254db7f9dd866a59f764aeb7fb8c4f1ed409ee
DatAsianBoi123 <zach.su158@gmail.com>
2022-10-21 11:51:59 -0600

    Added XP

---
+++ src/main/java/com/datasiqn/arcadia/util/XPUtil.java
@@ -0,0 +1,26 @@
+package com.datasiqn.arcadia.util;
+
+public final class XPUtil {
+    private XPUtil() {}
+
+    public static long getTotalXPAtLevel(int level) {
+        return (long) (100 * Math.pow(level, 2));
+    }
+
+    public static long getXPFromLevel(int from, int to) {
+        return getTotalXPAtLevel(to) - getTotalXPAtLevel(from);
+    }
+
+    public static long getXPForLevel(int level) {
+        return (long) 200 * (level + 1) - 100;
+    }
+
+    public static int getLevelFromXP(long xp) {
+        return (int) Math.floor(Math.sqrt(xp / 100d));
+    }
+
+    public static double getProgress(long xp) {
+        int level = getLevelFromXP(xp);
+        return (xp - getTotalXPAtLevel(level)) / (double) getXPForLevel(level);
+    }
+}
