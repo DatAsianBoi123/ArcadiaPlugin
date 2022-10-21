66b5e86c0ea13da6d705263c402951b849f460f5
DatAsianBoi123 <zach.su158@gmail.com>
2022-10-21 11:55:52 -0600

    Added comments showing namespaced key type

--- src/main/java/com/datasiqn/arcadia/ArcadiaKeys.java
+++ src/main/java/com/datasiqn/arcadia/ArcadiaKeys.java
@@ -2,18 +2,49 @@ package com.datasiqn.arcadia;
 
 import org.bukkit.NamespacedKey;
 
-public abstract class ArcadiaKeys {
+public final class ArcadiaKeys {
+    private ArcadiaKeys() {}
+
+    /**
+     * string
+     */
     public static final NamespacedKey ITEM_ID = Arcadia.getNK("id");
+    /**
+     * string
+     */
     public static final NamespacedKey ITEM_UUID = Arcadia.getNK("uuid");
+    /**
+     * byte
+     */
     public static final NamespacedKey ITEM_MATERIAL = Arcadia.getNK("material");
+    /**
+     * double
+     */
     public static final NamespacedKey ITEM_QUALITY_BONUS = Arcadia.getNK("quality_bonus");
 
+    /**
+     * EnchantsDataType
+     */
     public static final NamespacedKey ITEM_ENCHANTS = Arcadia.getNK("enchants");
+    /**
+     * string
+     */
     public static final NamespacedKey ENCHANT_ID = Arcadia.getNK("id");
+    /**
+     * int
+     */
     public static final NamespacedKey ENCHANT_LEVEL = Arcadia.getNK("level");
-
+    /**
+     * double
+     */
     public static final NamespacedKey ARROW_DAMAGE = Arcadia.getNK("arrow_damage");
 
+    /**
+     * byte
+     */
     public static final NamespacedKey CRAFTING_RESULT = Arcadia.getNK("crafting");
+    /**
+     * byte
+     */
     public static final NamespacedKey ANVIL_RESULT = Arcadia.getNK("anvil_result");
 }
