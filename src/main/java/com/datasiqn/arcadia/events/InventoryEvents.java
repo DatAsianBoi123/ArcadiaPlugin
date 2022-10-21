9f96c6fdaf09ee9ccb2e1969ad1127d6d0da086b
DatAsianBoi123 <zach.su158@gmail.com>
2022-10-21 12:01:44 -0600

    Added inventory open event

--- src/main/java/com/datasiqn/arcadia/events/InventoryEvents.java
+++ src/main/java/com/datasiqn/arcadia/events/InventoryEvents.java
@@ -6,6 +6,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class InventoryEvents implements Listener {
@@ -20,6 +21,13 @@ public class InventoryEvents implements Listener {
     public void onInventoryDrag(@NotNull InventoryDragEvent event) {
         if (event.getInventory().getHolder() instanceof ArcadiaGUI gui) {
             gui.clickEvent(event);
+        }
+    }
+
+    @EventHandler
+    public void onInventoryOpen(@NotNull InventoryOpenEvent event) {
+        if (event.getInventory().getHolder() instanceof ArcadiaGUI gui) {
+            gui.openEvent(event);
         }
     }
 
