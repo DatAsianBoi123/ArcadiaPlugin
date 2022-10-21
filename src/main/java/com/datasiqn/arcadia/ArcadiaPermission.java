8d2cd704e1ed7941da1465fd20eb9939e84f51b7
DatAsianBoi123 <zach.su158@gmail.com>
2022-10-21 12:04:27 -0600

    Created new permissions

--- src/main/java/com/datasiqn/arcadia/ArcadiaPermission.java
+++ src/main/java/com/datasiqn/arcadia/ArcadiaPermission.java
@@ -1,17 +1,17 @@
 package com.datasiqn.arcadia;
 
-import org.bukkit.ChatColor;
+public final class ArcadiaPermission {
+    private ArcadiaPermission() {}
 
-public abstract class ArcadiaPermission {
     public static final String PERMISSION_USE_HEAL = "arcadia.heal";
     public static final String PERMISSION_USE_ITEM = "arcadia.item";
     public static final String PERMISSION_USE_SUMMON = "arcadia.summon";
     public static final String PERMISSION_USE_GUI = "arcadia.gui";
-    public static final String PERMISSION_USE_HELP = "arcadia.help";
     public static final String PERMISSION_USE_DEBUG = "arcadia.debug";
+    public static final String PERMISSION_USE_CONFIG = "arcadia.config";
     public static final String PERMISSION_USE_RECIPE = "arcadia.recipe";
     public static final String PERMISSION_USE_LOOT = "arcadia.loot";
     public static final String PERMISSION_USE_ENCHANT = "arcadia.enchant";
-
-    public static final String MISSING_PERMISSIONS = ChatColor.RED + "You do not have permission to use this command.";
+    public static final String PERMISSION_MANAGE_DUNGEONS = "arcadia.dungeons";
+    public static final String PERMISSION_USE_LOBBY = "arcadia.lobby";
 }
