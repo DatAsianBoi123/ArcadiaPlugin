61be80635802e8f004f5f4904fb3a0cb8a1b665e
DatAsianBoi123 <zach.su158@gmail.com>
2022-10-22 12:34:41 -0600

    Added a loot table argument for spawning loot chests

--- src/main/java/com/datasiqn/arcadia/commands/CommandSpawn.java
+++ src/main/java/com/datasiqn/arcadia/commands/CommandSpawn.java
@@ -2,8 +2,11 @@ package com.datasiqn.arcadia.commands;
 
 import com.datasiqn.arcadia.ArcadiaKeys;
 import com.datasiqn.arcadia.ArcadiaPermission;
+import com.datasiqn.arcadia.commands.arguments.ArcadiaArgumentType;
+import com.datasiqn.arcadia.loottables.ArcadiaLootTable;
 import com.datasiqn.arcadia.loottables.LootTables;
 import com.datasiqn.commandcore.commands.Command;
+import com.datasiqn.commandcore.commands.builder.ArgumentBuilder;
 import com.datasiqn.commandcore.commands.builder.CommandBuilder;
 import com.datasiqn.commandcore.commands.builder.LiteralBuilder;
 import org.bukkit.Location;
@@ -14,6 +17,7 @@ import org.bukkit.block.EnderChest;
 import org.bukkit.entity.Player;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
+import org.jetbrains.annotations.NotNull;
 
 public class CommandSpawn {
     public Command getCommand() {
@@ -30,16 +34,19 @@ public class CommandSpawn {
                             enderChest.update();
                         }))
                 .then(LiteralBuilder.<Player>literal("lootchest")
-                        .executes(context -> {
-                            Player player = context.getSender();
-                            Location location = player.getLocation();
-                            World world = player.getWorld();
-                            world.setType(location, Material.CHEST);
-                            Chest chest = (Chest) world.getBlockAt(location).getState();
-                            PersistentDataContainer pdc = chest.getPersistentDataContainer();
-                            pdc.set(ArcadiaKeys.LOOT_TABLE, PersistentDataType.STRING, LootTables.CHEST_DEFAULT.name());
-                            chest.update();
-                        }))
+                        .then(ArgumentBuilder.<Player, LootTables>argument(ArcadiaArgumentType.LOOT_TABLE, "loot table")
+                                .executes(context -> spawnLootChest(context.getSender().getLocation(), context.parseArgument(ArcadiaArgumentType.LOOT_TABLE, 1).getLootTable())))
+                        .executes(context -> spawnLootChest(context.getSender().getLocation(), LootTables.CHEST_DEFAULT.getLootTable())))
                 .build();
+    }
+
+    private void spawnLootChest(@NotNull Location location, ArcadiaLootTable lootTable) {
+        World world = location.getWorld();
+        if (world == null) return;
+        world.setType(location, Material.CHEST);
+        Chest chest = (Chest) world.getBlockAt(location).getState();
+        PersistentDataContainer pdc = chest.getPersistentDataContainer();
+        pdc.set(ArcadiaKeys.LOOT_TABLE, PersistentDataType.STRING, lootTable.getId());
+        chest.update();
     }
 }
