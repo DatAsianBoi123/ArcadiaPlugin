cfe1ceb9fd2eb94859e522c9e3696056c920a5c5
DatAsianBoi123 <zach.su158@gmail.com>
2022-10-22 12:14:54 -0600

    Added permissions

--- src/main/java/com/datasiqn/arcadia/ArcadiaPermission.java
+++ src/main/java/com/datasiqn/arcadia/ArcadiaPermission.java
@@ -6,9 +6,9 @@ public final class ArcadiaPermission {
     public static final String PERMISSION_USE_HEAL = "arcadia.heal";
     public static final String PERMISSION_USE_ITEM = "arcadia.item";
     public static final String PERMISSION_USE_SUMMON = "arcadia.summon";
+    public static final String PERMISSION_USE_SPAWN = "arcadia.spawn";
     public static final String PERMISSION_USE_GUI = "arcadia.gui";
     public static final String PERMISSION_USE_DEBUG = "arcadia.debug";
-    public static final String PERMISSION_USE_CONFIG = "arcadia.config";
     public static final String PERMISSION_USE_RECIPE = "arcadia.recipe";
     public static final String PERMISSION_USE_LOOT = "arcadia.loot";
     public static final String PERMISSION_USE_ENCHANT = "arcadia.enchant";
