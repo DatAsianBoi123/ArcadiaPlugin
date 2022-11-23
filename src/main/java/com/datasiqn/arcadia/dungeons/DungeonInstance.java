908fd5260e2a4b335d98aae7fc3f7fc90d164f0b
DatAsianBoi123 <zach.su158@gmail.com>
2022-11-23 11:51:21 -0700

    converted to class

--- src/main/java/com/datasiqn/arcadia/dungeons/DungeonInstance.java
+++ src/main/java/com/datasiqn/arcadia/dungeons/DungeonInstance.java
@@ -1,6 +1,30 @@
 package com.datasiqn.arcadia.dungeons;
 
 import org.bukkit.World;
+import org.bukkit.entity.Player;
 
-public record DungeonInstance(World world, String id) {
+import java.util.HashSet;
+import java.util.Set;
+
+public final class DungeonInstance {
+    private final Set<DungeonPlayer> players = new HashSet<>();
+    private final World world;
+    private final String id;
+
+    public DungeonInstance(World world, String id) {
+        this.world = world;
+        this.id = id;
+    }
+
+    public World getWorld() {
+        return world;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void addPlayer(Player player) {
+
+    }
 }
