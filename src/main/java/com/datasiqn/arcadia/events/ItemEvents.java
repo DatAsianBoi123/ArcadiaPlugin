4236b22ab6615a640403990b6ad97e3c849e24e5
DatAsianBoi123 <zach.su158@gmail.com>
2022-11-23 11:54:18 -0700

    added a beta bag open feature

--- src/main/java/com/datasiqn/arcadia/events/ItemEvents.java
+++ src/main/java/com/datasiqn/arcadia/events/ItemEvents.java
@@ -1,18 +1,30 @@
 package com.datasiqn.arcadia.events;
 
 import com.datasiqn.arcadia.Arcadia;
+import com.datasiqn.arcadia.ArcadiaKeys;
+import com.datasiqn.arcadia.datatype.ArcadiaDataType;
+import com.datasiqn.arcadia.guis.StaticGUI;
 import com.datasiqn.arcadia.items.ArcadiaItem;
 import com.datasiqn.arcadia.items.abilities.AbilityExecutor;
 import com.datasiqn.arcadia.items.abilities.ItemAbility;
 import com.datasiqn.arcadia.players.PlayerData;
+import com.datasiqn.arcadia.util.ItemUtil;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
 import org.jetbrains.annotations.NotNull;
 
 import java.text.DecimalFormat;
@@ -26,6 +38,37 @@ public class ItemEvents implements Listener {
 
     public ItemEvents(Arcadia plugin) {
         this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerOpenBag(@NotNull InventoryClickEvent event) {
+        Inventory inventory = event.getClickedInventory();
+        if (inventory == null) return;
+        InventoryHolder holder = inventory.getHolder();
+        if (holder == null) return;
+        HumanEntity whoClicked = event.getWhoClicked();
+        if (!holder.equals(whoClicked)) return;
+
+        ItemStack item = event.getCurrentItem();
+        if (item == null || item.getType() == Material.AIR) return;
+        ItemMeta meta = item.getItemMeta();
+        if (meta == null) return;
+        PersistentDataContainer pdc = meta.getPersistentDataContainer();
+        if (!pdc.getOrDefault(ArcadiaKeys.UPGRADE_BAG, ArcadiaDataType.BOOLEAN, false)) return;
+        event.setCancelled(true);
+        Inventory bagInventory = new StaticGUI(54, "Item Bag") {}.getInventory();
+        ItemStack empty = ItemUtil.createEmpty(Material.GRAY_STAINED_GLASS_PANE);
+        ItemStack closeItem = new ItemStack(Material.BARRIER);
+        ItemMeta closeMeta = closeItem.getItemMeta();
+        if (closeMeta == null) return;
+        closeMeta.setDisplayName(ChatColor.RED + "Close");
+        closeItem.setItemMeta(closeMeta);
+        for (int i = 0; i < 9; i++) bagInventory.setItem(53 - i, empty);
+        bagInventory.setItem(49, closeItem);
+        plugin.runAfterOneTick(() -> {
+            whoClicked.getWorld().playSound(whoClicked, Sound.BLOCK_CHEST_OPEN, 1, 1);
+            whoClicked.openInventory(bagInventory);
+        });
     }
 
     @EventHandler
