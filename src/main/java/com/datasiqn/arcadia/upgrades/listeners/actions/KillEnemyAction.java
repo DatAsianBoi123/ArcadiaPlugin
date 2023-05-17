fab8f36a78ed96d6d54ab4e199fa290a48fc8d94
DatAsianBoi123 <zach.su158@gmail.com>
2023-05-16 20:07:27 -0600

    added kill enemy action

---
+++ src/main/java/com/datasiqn/arcadia/upgrades/listeners/actions/KillEnemyAction.java
@@ -0,0 +1,10 @@
+package com.datasiqn.arcadia.upgrades.listeners.actions;
+
+import com.datasiqn.arcadia.dungeons.DungeonPlayer;
+import com.datasiqn.arcadia.entities.ArcadiaEntity;
+
+public class KillEnemyAction extends EntityAction {
+    public KillEnemyAction(DungeonPlayer player, ArcadiaEntity entity) {
+        super(player, entity);
+    }
+}
