27fd21142809ff30b1f230ae4c7faf71fb05b2f2
DatAsianBoi123 <zach.su158@gmail.com>
2023-08-13 11:02:33 -0600

    created attribute argument type

--- src/main/java/com/datasiqn/arcadia/commands/argument/ArcadiaArgumentType.java
+++ src/main/java/com/datasiqn/arcadia/commands/argument/ArcadiaArgumentType.java
@@ -6,6 +6,7 @@ import com.datasiqn.arcadia.entities.EntityType;
 import com.datasiqn.arcadia.item.material.ArcadiaMaterial;
 import com.datasiqn.arcadia.loottable.LootTable;
 import com.datasiqn.arcadia.menu.MenuType;
+import com.datasiqn.arcadia.player.PlayerAttribute;
 import com.datasiqn.arcadia.player.PlayerData;
 import com.datasiqn.arcadia.recipe.ArcadiaRecipe;
 import com.datasiqn.arcadia.upgrade.UpgradeType;
@@ -16,19 +17,21 @@ public final class ArcadiaArgumentType {
 
     public static final ArgumentType<PlayerData> PLAYER = new PlayerArgumentType();
 
-    public static final ArgumentType<ArcadiaMaterial> ITEM = new ArgumentType.EnumArgumentType<>(ArcadiaMaterial.class);
+    public static final ArgumentType<PlayerAttribute> PLAYER_ATTRIBUTE = new ArgumentType.EnumArgumentType<>(PlayerAttribute.class, "attribute");
 
-    public static final ArgumentType<EnchantType> ENCHANT = new ArgumentType.EnumArgumentType<>(EnchantType.class);
+    public static final ArgumentType<ArcadiaMaterial> ITEM = new ArgumentType.EnumArgumentType<>(ArcadiaMaterial.class, "item");
 
-    public static final ArgumentType<LootTable> LOOT_TABLE = new ArgumentType.EnumArgumentType<>(LootTable.class);
+    public static final ArgumentType<EnchantType> ENCHANT = new ArgumentType.EnumArgumentType<>(EnchantType.class, "enchant");
 
-    public static final ArgumentType<ArcadiaRecipe> RECIPE = new ArgumentType.EnumArgumentType<>(ArcadiaRecipe.class);
+    public static final ArgumentType<LootTable> LOOT_TABLE = new ArgumentType.EnumArgumentType<>(LootTable.class, "loot table");
 
-    public static final ArgumentType<UpgradeType> UPGRADE = new ArgumentType.EnumArgumentType<>(UpgradeType.class);
+    public static final ArgumentType<ArcadiaRecipe> RECIPE = new ArgumentType.EnumArgumentType<>(ArcadiaRecipe.class, "recipe");
 
-    public static final ArgumentType<EntityType> ENTITY = new ArgumentType.EnumArgumentType<>(EntityType.class);
+    public static final ArgumentType<UpgradeType> UPGRADE = new ArgumentType.EnumArgumentType<>(UpgradeType.class, "upgrade");
 
-    public static final ArgumentType<MenuType> GUI = new ArgumentType.EnumArgumentType<>(MenuType.class);
+    public static final ArgumentType<EntityType> ENTITY = new ArgumentType.EnumArgumentType<>(EntityType.class, "entity");
+
+    public static final ArgumentType<MenuType> GUI = new ArgumentType.EnumArgumentType<>(MenuType.class, "menu");
 
     public static final ArgumentType<DungeonInstance> DUNGEON = new DungeonArgumentType();
 }
