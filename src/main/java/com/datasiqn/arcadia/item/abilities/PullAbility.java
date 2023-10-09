64906aea80e9f77d64af8a3f8a5668aba581d66a
DatAsianBoi123 <zach.su158@gmail.com>
2023-10-08 18:41:53 -0600

    updated Pull ability

--- src/main/java/com/datasiqn/arcadia/item/abilities/PullAbility.java
+++ src/main/java/com/datasiqn/arcadia/item/abilities/PullAbility.java
@@ -15,7 +15,7 @@ public class PullAbility extends ItemAbility {
     @Override
     public void execute(@NotNull AbilityExecutor executor) {
         Player player = executor.playerData().getSender().get();
-        player.setVelocity(player.getVelocity().add(player.getLocation().getDirection().multiply(2).add(new Vector(0, 0.75, 0))));
+        player.setVelocity(player.getVelocity().setY(0).add(player.getLocation().getDirection().multiply(2).add(new Vector(0, 0.75, 0))));
         player.playSound(player, Sound.ENTITY_GENERIC_EXPLODE, 0.5f, 1);
         player.spawnParticle(Particle.CLOUD, player.getEyeLocation().add(player.getLocation().getDirection().multiply(0.1)), 15, 0.01, 0.01, 0.01, 0.1);
     }
