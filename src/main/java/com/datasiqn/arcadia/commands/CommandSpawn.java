4b23ac88124c3d2211ae1ebc5ee11db9b120d724
DatAsianBoi123 <zach.su158@gmail.com>
2023-10-08 18:41:17 -0600

    used updated methods in CommandCore

--- src/main/java/com/datasiqn/arcadia/commands/CommandSpawn.java
+++ src/main/java/com/datasiqn/arcadia/commands/CommandSpawn.java
@@ -8,12 +8,12 @@ import com.datasiqn.arcadia.util.PdcUtil;
 import com.datasiqn.commandcore.command.builder.ArgumentBuilder;
 import com.datasiqn.commandcore.command.builder.CommandBuilder;
 import com.datasiqn.commandcore.command.builder.LiteralBuilder;
+import com.datasiqn.commandcore.locatable.LocatableCommandSender;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Chest;
 import org.bukkit.block.EnderChest;
-import org.bukkit.entity.Player;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.jetbrains.annotations.NotNull;
 
@@ -23,11 +23,11 @@ public class CommandSpawn {
                 .permission(ArcadiaPermission.PERMISSION_USE_SPAWN)
                 .description("Spawns different types of chests")
                 .then(LiteralBuilder.literal("upgradechest")
-                        .requiresPlayer()
+                        .requiresLocatable()
                         .executes(context -> {
-                            Player player = context.getSource().getPlayer();
-                            Location location = player.getLocation();
-                            World world = player.getWorld();
+                            LocatableCommandSender locatable = context.getSource().getLocatable();
+                            Location location = locatable.getLocation();
+                            World world = locatable.getWorld();
                             world.setType(location, Material.ENDER_CHEST);
                             EnderChest enderChest = (EnderChest) world.getBlockAt(location).getState();
                             PersistentDataContainer pdc = enderChest.getPersistentDataContainer();
@@ -36,10 +36,10 @@ public class CommandSpawn {
                         }))
                 .then(LiteralBuilder.literal("lootchest")
                         .then(ArgumentBuilder.argument(ArcadiaArgumentType.LOOT_TABLE, "loot table")
-                                .requiresPlayer()
+                                .requiresLocatable()
                                 .executes(context -> {
                                     LootTable lootTable = context.getArguments().get(1, ArcadiaArgumentType.LOOT_TABLE);
-                                    spawnLootChest(context.getSource().getPlayer().getLocation(), lootTable);
+                                    spawnLootChest(context.getSource().getLocatable().getLocation(), lootTable);
                                 }))
                         .requiresPlayer()
                         .executes(context -> spawnLootChest(context.getSource().getPlayer().getLocation(), LootTable.CHEST_DEFAULT)));
--- src/main/java/com/datasiqn/arcadia/commands/CommandSummon.java
+++ src/main/java/com/datasiqn/arcadia/commands/CommandSummon.java
@@ -1,17 +1,24 @@
 package com.datasiqn.arcadia.commands;
 
+import com.datasiqn.arcadia.Arcadia;
 import com.datasiqn.arcadia.ArcadiaPermission;
 import com.datasiqn.arcadia.commands.argument.ArcadiaArgumentType;
 import com.datasiqn.commandcore.command.builder.ArgumentBuilder;
 import com.datasiqn.commandcore.command.builder.CommandBuilder;
 
 public class CommandSummon {
+    private final Arcadia plugin;
+
+    public CommandSummon(Arcadia plugin) {
+        this.plugin = plugin;
+    }
+
     public CommandBuilder getCommand() {
         return new CommandBuilder("summon")
                 .permission(ArcadiaPermission.PERMISSION_USE_SUMMON)
                 .description("Summons a custom Arcadia Entity")
                 .then(ArgumentBuilder.argument(ArcadiaArgumentType.ENTITY, "entity")
-                        .requiresPlayer()
-                        .executes(context -> context.getArguments().get(0, ArcadiaArgumentType.ENTITY).getSummoner().summonEntity(context.getSource().getPlayer().getLocation())));
+                        .requiresLocatable()
+                        .executes(context -> context.getArguments().get(0, ArcadiaArgumentType.ENTITY).getSummoner().summonEntity(context.getSource().getLocatable().getLocation(), plugin)));
     }
 }
