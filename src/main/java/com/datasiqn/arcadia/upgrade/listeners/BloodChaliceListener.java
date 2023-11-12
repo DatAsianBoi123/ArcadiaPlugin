54e0c3290609665fced9cb68ce1fb604eeaeb390
DatAsianBoi123 <zach.su158@gmail.com>
2023-11-12 16:13:10 -0700

    made blood chalice only give health when a hostile entity is killed

--- src/main/java/com/datasiqn/arcadia/upgrade/listeners/BloodChaliceListener.java
+++ src/main/java/com/datasiqn/arcadia/upgrade/listeners/BloodChaliceListener.java
@@ -1,11 +1,13 @@
 package com.datasiqn.arcadia.upgrade.listeners;
 
+import com.datasiqn.arcadia.entities.ArcadiaHostileEntity;
 import com.datasiqn.arcadia.upgrade.listeners.actions.KillEnemyAction;
 import org.jetbrains.annotations.NotNull;
 
 public class BloodChaliceListener implements UpgradeListener {
     @ActionHandler(priority = 10)
     public void onKill(@NotNull KillEnemyAction action, int stackAmount) {
+        if (!(action.getEntity() instanceof ArcadiaHostileEntity)) return;
         action.getPlayer().getPlayerData().heal(10 + 5 * stackAmount);
     }
 }
