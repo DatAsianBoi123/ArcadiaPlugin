9f245d9fe656def0c4013d36e428b5e04a086998
DatAsianBoi123 <zach.su158@gmail.com>
2023-12-02 21:41:00 -0700

    made AbilityExecutor a record instead of an interface

--- src/main/java/com/datasiqn/arcadia/item/abilities/AbilityExecutor.java
+++ src/main/java/com/datasiqn/arcadia/item/abilities/AbilityExecutor.java
@@ -1,9 +1,6 @@
 package com.datasiqn.arcadia.item.abilities;
 
 import com.datasiqn.arcadia.player.PlayerData;
-import org.jetbrains.annotations.NotNull;
 
-public interface AbilityExecutor {
-    @NotNull PlayerData playerData();
-    @NotNull ItemAbility ability();
+public record AbilityExecutor(PlayerData playerData, ItemAbility ability) {
 }
