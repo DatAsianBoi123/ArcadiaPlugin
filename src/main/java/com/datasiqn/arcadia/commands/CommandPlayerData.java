0d492c1836a627a4049b316bc483c2f913335ee0
DatAsianBoi123 <zach.su158@gmail.com>
2024-03-21 19:15:21 -0600

    added playerdata xp command

--- src/main/java/com/datasiqn/arcadia/commands/CommandPlayerData.java
+++ src/main/java/com/datasiqn/arcadia/commands/CommandPlayerData.java
@@ -5,6 +5,7 @@ import com.datasiqn.arcadia.ArcadiaPermission;
 import com.datasiqn.arcadia.commands.argument.ArcadiaArgumentType;
 import com.datasiqn.arcadia.managers.PlayerManager;
 import com.datasiqn.arcadia.player.PlayerData;
+import com.datasiqn.commandcore.argument.type.ArgumentType;
 import com.datasiqn.commandcore.command.builder.ArgumentBuilder;
 import com.datasiqn.commandcore.command.builder.CommandBuilder;
 import com.datasiqn.commandcore.command.builder.LiteralBuilder;
@@ -19,9 +20,11 @@ import java.util.concurrent.Executors;
 public class CommandPlayerData {
     private static final DecimalFormat ATTRIBUTE_FORMAT = new DecimalFormat("#.##");
 
+    private final Arcadia plugin;
     private final PlayerManager playerManager;
 
     public CommandPlayerData(@NotNull Arcadia plugin) {
+        this.plugin = plugin;
         this.playerManager = plugin.getPlayerManager();
     }
 
@@ -31,11 +34,11 @@ public class CommandPlayerData {
                 .description("Manages player data")
                 .then(LiteralBuilder.literal("load")
                         .then(ArgumentBuilder.argument(ArcadiaArgumentType.PLAYER, "player")
-                                .executes((context, source, arguments) -> new Thread(() -> {
+                                .executesAsync((context, source, arguments) -> {
                                     PlayerData playerData = arguments.get(1, ArcadiaArgumentType.PLAYER);
                                     playerData.loadData();
                                     playerData.getSender().sendMessage("Successfully loaded " + playerData.getPlayer().getName() + "'s player data");
-                                }).start()))
+                                }))
                         .executes((context, source, arguments) -> {
                             ExecutorService threadPool = Executors.newFixedThreadPool(4);
                             for (Player player : Bukkit.getOnlinePlayers()) {
@@ -47,11 +50,11 @@ public class CommandPlayerData {
                         }))
                 .then(LiteralBuilder.literal("save")
                         .then(ArgumentBuilder.argument(ArcadiaArgumentType.PLAYER, "player")
-                                .executes((context, source, arguments) -> new Thread(() -> {
+                                .executesAsync((context, source, arguments) -> {
                                     PlayerData playerData = arguments.get(1, ArcadiaArgumentType.PLAYER);
                                     playerData.saveData();
                                     playerData.getSender().sendMessage("Successfully saved " + playerData.getPlayer().getName() + "'s player data");
-                                }).start()))
+                                }))
                         .executes((context, source, arguments) -> {
                             ExecutorService threadPool = Executors.newFixedThreadPool(4);
                             for (Player player : Bukkit.getOnlinePlayers()) {
@@ -68,6 +71,16 @@ public class CommandPlayerData {
                                         .executes((context, source, arguments) -> {
                                             PlayerData playerData = playerManager.getPlayerData(source.getPlayer());
                                             playerData.getSender().sendMessage(ATTRIBUTE_FORMAT.format(playerData.getAttribute(arguments.get(2, ArcadiaArgumentType.PLAYER_ATTRIBUTE))));
+                                        }))))
+                .then(LiteralBuilder.literal("xp")
+                        .then(LiteralBuilder.literal("set")
+                                .then(ArgumentBuilder.argument(ArgumentType.boundedNumber(long.class, 0L), "amount")
+                                        .requiresPlayer()
+                                        .executes((context, source, arguments) -> {
+                                            Player player = source.getPlayer();
+                                            PlayerData playerData = playerManager.getPlayerData(player);
+                                            playerData.getXp().setAmount(arguments.get(2, ArgumentType.number(long.class)));
+                                            plugin.getScoreboardManager().updateScoreboard(player);
                                         }))));
     }
 }
