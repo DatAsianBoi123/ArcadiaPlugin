6df08b6adf054589aa1b490dd2d5feefdba96064
DatAsianBoi123 <zach.su158@gmail.com>
2024-06-01 23:37:41 -0600

    updated CommandCore

--- src/main/java/com/datasiqn/arcadia/commands/argument/ArcadiaArgumentType.java
+++ src/main/java/com/datasiqn/arcadia/commands/argument/ArcadiaArgumentType.java
@@ -11,25 +11,26 @@ import com.datasiqn.arcadia.player.PlayerData;
 import com.datasiqn.arcadia.recipe.ArcadiaRecipe;
 import com.datasiqn.arcadia.upgrade.UpgradeType;
 import com.datasiqn.commandcore.argument.type.ArgumentType;
+import com.datasiqn.commandcore.argument.type.EnumArgumentType;
 
 public final class ArcadiaArgumentType {
     private ArcadiaArgumentType() { }
 
     public static final ArgumentType<PlayerData> PLAYER = new PlayerArgumentType();
 
-    public static final ArgumentType<PlayerAttribute> PLAYER_ATTRIBUTE = new ArgumentType.EnumArgumentType<>(PlayerAttribute.class, "attribute");
+    public static final ArgumentType<PlayerAttribute> PLAYER_ATTRIBUTE = new EnumArgumentType<>(PlayerAttribute.class, "attribute");
 
-    public static final ArgumentType<ArcadiaMaterial> ITEM = new ArgumentType.EnumArgumentType<>(ArcadiaMaterial.class, "item");
+    public static final ArgumentType<ArcadiaMaterial> ITEM = new EnumArgumentType<>(ArcadiaMaterial.class, "item");
 
-    public static final ArgumentType<LootTable> LOOT_TABLE = new ArgumentType.EnumArgumentType<>(LootTable.class, "loot table");
+    public static final ArgumentType<LootTable> LOOT_TABLE = new EnumArgumentType<>(LootTable.class, "loot table");
 
-    public static final ArgumentType<ArcadiaRecipe> RECIPE = new ArgumentType.EnumArgumentType<>(ArcadiaRecipe.class, "recipe");
+    public static final ArgumentType<ArcadiaRecipe> RECIPE = new EnumArgumentType<>(ArcadiaRecipe.class, "recipe");
 
-    public static final ArgumentType<UpgradeType> UPGRADE = new ArgumentType.EnumArgumentType<>(UpgradeType.class, "upgrade");
+    public static final ArgumentType<UpgradeType> UPGRADE = new EnumArgumentType<>(UpgradeType.class, "upgrade");
 
-    public static final ArgumentType<EntityType> ENTITY = new ArgumentType.EnumArgumentType<>(EntityType.class, "entity");
+    public static final ArgumentType<EntityType> ENTITY = new EnumArgumentType<>(EntityType.class, "entity");
 
-    public static final ArgumentType<MenuType> GUI = new ArgumentType.EnumArgumentType<>(MenuType.class, "menu");
+    public static final ArgumentType<MenuType> GUI = new EnumArgumentType<>(MenuType.class, "menu");
 
     public static final ArgumentType<DungeonInstance> DUNGEON = new DungeonArgumentType();
 
