5d5461d30198751a18e0c149823edc39a17ca371
DatAsianBoi123 <zach.su158@gmail.com>
2024-09-02 13:23:39 -0600

    updated upgrade compressor damage calculations it no longer takes in account stacks of itself and the damage multiplier is linear

--- src/main/java/com/datasiqn/arcadia/upgrade/listeners/UpgradeCompressorListener.java
+++ src/main/java/com/datasiqn/arcadia/upgrade/listeners/UpgradeCompressorListener.java
@@ -2,6 +2,8 @@ package com.datasiqn.arcadia.upgrade.listeners;
 
 import com.datasiqn.arcadia.dungeon.DungeonPlayer;
 import com.datasiqn.arcadia.player.ArcadiaSender;
+import com.datasiqn.arcadia.upgrade.Upgrade;
+import com.datasiqn.arcadia.upgrade.UpgradeType;
 import com.datasiqn.arcadia.upgrade.actions.DamageEnemyAction;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -12,8 +14,11 @@ public class UpgradeCompressorListener implements UpgradeListener {
     @ActionHandler(priority = 0)
     public void onDamage(@NotNull DamageEnemyAction action, int stackSize) {
         DungeonPlayer dungeonPlayer = action.getPlayer();
-        int totalUpgrades = dungeonPlayer.getTotalUpgrades();
-        double damageMultiplier = (stackSize / 4d) * Math.sqrt(totalUpgrades) + 1;
+        int totalUpgrades = dungeonPlayer.getUpgrades().stream()
+                .filter(upgrade -> upgrade.getType() != UpgradeType.UPGRADE_COMPRESSOR)
+                .mapToInt(Upgrade::getAmount)
+                .sum();
+        double damageMultiplier = 0.05 * stackSize * totalUpgrades + 1;
 
         if (dungeonPlayer.getPlayerData().inDebugMode()) {
             DecimalFormat format = new DecimalFormat("#,###.##");
